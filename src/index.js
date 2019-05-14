ccb2f681c506de1ab0dc829ae9defadfe488007e
weijujiang <weijujiang@hk01.com>
2019-05-14 15:04:20 +0800

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOMServer from 'react-dom/server';
 
 const ICE_CONTAINER = document.getElementById('ice-container');
 
+
 if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
