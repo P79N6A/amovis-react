a0d2a0b67010e98cb601aaa037f3f66461bd9b15
涂峰旗 <185441127@qq.com>
2019-05-31 16:55:28 +0800

    前端接口对接

--- src/pages/CommonPage/index.js
+++ src/pages/CommonPage/index.js
@@ -9,7 +9,9 @@ const cre = React.createElement
 export default class CommonPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      page_module_data: {}
+    };
   }
 
   componentDidMount = () => {
@@ -18,10 +20,11 @@ export default class CommonPage extends Component {
     headers.append("Authenticate", 10001);
     headers.append("Content-Type", "application/json");
     var modules = [];
+    var self = this;
     Object.values(this.props.data).map(function (part_modules, page_part) {
-      part_modules.map(function (items, type_key) {
-        Object.values(items).map(function (json_items) {
-          Object.values(json_items).map(function (json_item) {
+      part_modules.map(function (items, i) {
+        Object.values(items).map(function (json_items, type_key) {
+          Object.values(json_items).map(function (json_item, j) {
             modules.push(json_item);
           });
         });
@@ -31,15 +34,37 @@ export default class CommonPage extends Component {
       "modules": modules,
       "is_advance": 0
     };
-    UtilFetch.post(fetchUrl, JSON.stringify(formData), headers).then(function (res) {
-      console.log(res);
+    UtilFetch.post(fetchUrl, JSON.stringify(formData), headers).then((res) => {
+      this.setState({ page_module_data: res.val });
+      // 重新设计 this.props.data 中module_data数据，防止页面返回的时候读取的默认数据出现闪烁
+      for (let page_part in this.props.data) {
+        let part_modules = this.props.data[page_part];
+        for (let i in part_modules) {
+          let items = part_modules[i];
+          for (let type_key in items) {
+            let json_items = items[type_key];
+            for (let j in json_items) {
+              self.modProps(page_part, i, type_key, j, res.val);
+            }
+          }
+        }
+      }
     });
+  }
+  modProps = (page_part, i, type_key, j, val) => {
+    var item = this.props.data[page_part][i][type_key][j];
+    var key = item.project_name + "/" + item.module_name + "/" + item.page_name + "/" + item.position;
+    this.props.data[page_part][i][type_key][j].module_data = val[key];
   }
   getDetail = (sectionItem) => {
     return Object.values(sectionItem).map((cn, j) => {
-      let projectName = cn.project_name
-      let moduleName = cn.module_name
-      let moduleData = cn.module_data
+      let projectName = cn.project_name;
+      let moduleName = cn.module_name;
+      let key = cn.project_name + "/" + cn.module_name + "/" + cn.page_name + "/" + cn.position;
+      let moduleData = cn.module_data;
+      if (this.state.page_module_data[key]) {
+        moduleData = this.state.page_module_data[key];
+      }
       return (
         cre(B[projectName][moduleName], { key: j, "data-key": j, ...moduleData }, null)
       )
